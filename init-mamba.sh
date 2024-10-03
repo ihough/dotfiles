bc91af6990f37c13bad434ea39f3fd31f6edf9f7
Ian Hough <ian.hough@univ-grenoble-alpes.fr>
2024-10-03 12:33:07 +0200

    Simplify init-mamba.sh

--- init-mamba.sh
+++ init-mamba.sh
@@ -5,23 +5,13 @@
 
 echo "Activating micromamba"
 
-# >>> mamba initialize >>>
-# !! Contents within this block are managed by 'mamba init' !!
-export MAMBA_EXE='/home/houghi/.local/bin/micromamba';
-export MAMBA_ROOT_PREFIX='/home/houghi/micromamba';
-__mamba_setup="$("$MAMBA_EXE" shell hook --shell bash --root-prefix "$MAMBA_ROOT_PREFIX" 2> /dev/null)"
-if [ $? -eq 0 ]; then
-    eval "$__mamba_setup"
-else
-    alias micromamba="$MAMBA_EXE"  # Fallback on help from mamba activate
-fi
-unset __mamba_setup
-# <<< mamba initialize <<<
-
-# Enable tab completion for `mm` by marking it as an alias for `micromamba`
-if type _umamba_bash_completions &> /dev/null; then
-  complete -o default -F _umamba_bash_completions mm
-fi
+# Parse and eval the output of `micromamba shell init --dry-run`
+__mamba_shell_init="$(
+  micromamba shell init --dry-run \
+    | sed -n '/# >>> mamba initialize >>>$/,/^# <<< mamba initialize <<<$/p'
+)"
+eval "$__mamba_shell_init"
+unset __mamba_shell_init
 
 # Activate environment if specified
 if ! [ -z "$1" ]; then
